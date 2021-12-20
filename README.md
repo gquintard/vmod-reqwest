e8de3f856801738bc73eb1b73f68c9b4d5a4c8e1
Guillaume Quintard <guillaume.quintard@gmail.com>
2021-12-19 18:30:58 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-This repo is a quick snapshot of https://github.com/gquintard/varnish-rs/tree/main/examples/vmod_example slightly tweaked to be easy to clone and get going in case you need to create a new vmod in Rust.
+# vmod_reqwest
+
+This is a vmod for [varnish](http://varnish-cache.org/) to send and receive HTTP requests from VCL, leveraging the [reqwest crate](https://docs.rs/reqwest/latest/reqwest/).
+
+It offers similar features to [vmod_curl](https://github.com/varnish/libvmod-curl), but notably offers fire-and-forget capabilities and parallel request handling.
+
+As usual, the full VCL API is described in [vmod.vcc](vmod.vcc).
+
+## VCL Example
+
+``` bash
+import request;
+
+sub vcl_recv {
+	# send a request into the void and don't worry if it complete or not
+	reqwest.init("async", "POST", "https://api.example.com/log")
+	reqwest.set_body("URL = " + req.url);
+	reqwest.send();
+
+	# use an HTTP request to grant (or not) access to the client
+	reqwest.init("sync", "GET", "https://api.example.com/authorized/" + req.http.user);
+	if (reqwest.status("sync") == 200) {
+		return (lookup);
+	} else {
+		return (synth(403));
+	}
+}
+```
+
+## Requirements
+
+You'll need:
+- `cargo` (and the accompanying `rust` package)
+- `python3`
+- the `varnish` 7.0.1 development libraries/headers ([depends on the `varnish` crate you are using](https://github.com/gquintard/varnish-rs#versions))
+
+## Build and test
+
+``` bash
+cargo build --release
+cargo test --release
+```
+
+The vmod file will be found at `target/release/libvmod_reqwest.so`.
+
+## Packages
+
+To avoid making a mess of your system, you probably should install your vmod as a proper package. This repository also offers different templates, and some quick recipes for different distributions.
+
+### All platforms
+
+First it's necessary to set the `VMOD_VERSION` (the version of this vmod) and `VARNISH_VERSION` (the Varnish version to build against) environment variables. It can be done manually, or using `cargo` and `jq`:
+``` bash
+VMOD_VERSION=$(cargo metadata --no-deps --format-version 1 | jq '.packages[0].version' -r)
+VARNISH_MINOR=$(cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "varnish-sys") | .metadata.libvarnishapi.version ')
+VARNISH_PATCH=0
+VARNISH_VERSION="$VARNISH_MINOR.$VARNISH_PATCH"
+
+# or
+VMOD_VERSION=0.0.1
+VARNISH_VERSION=7.0.0
+```
+
+Then create the dist tarball, for example using `git archive`:
+
+``` bash
+git archive --output=vmod_reqwest-$VMOD_VERSION.tar.gz --format=tar.gz HEAD
+```
+
+Then, follow distribution-specific instructions.
+
+### Arch
+
+``` bash
+# create a work directory
+mkdir build
+# copy the tarball and PKGBUIL file, substituing the variables we care about
+cp vmod_reqwest-$VMOD_VERSION.tar.gz build
+sed -e "s/@VMOD_VERSION@/$VMOD_VERSION/" -e "s/@VARNISH_VERSION@/$VARNISH_VERSION/" pkg/arch/PKGBUILD > build/PKGBUILD
+
+# build
+cd build
+makepkg -rsf
+```
+
+Your package will be the file with the `.pkg.tar.zst` extension in `build/`
+
+### Alpine
+
+Alpine needs a bit of setup on the first time, but the [documentation](https://wiki.alpinelinux.org/wiki/Creating_an_Alpine_package) is excellent.
+
+``` bash
+# install some packages, create a user, give it power and a key
+apk add -q --no-progress --update tar alpine-sdk sudo
+adduser -D builder
+echo "builder ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
+addgroup builder abuild
+su builder -c "abuild-keygen -nai"
+```
+
+Then, to actually build your package:
+
+``` bash
+# create a work directory
+mkdir build
+# copy the tarball and PKGBUIL file, substituing the variables we care about
+cp vmod_reqwest-$VMOD_VERSION.tar.gz build
+sed -e "s/@VMOD_VERSION@/$VMOD_VERSION/" -e "s/@VARNISH_VERSION@/$VARNISH_VERSION/" pkg/arch/APKBUILD > build/APKBUILD
+
+su builder -c "abuild checksum"
+su builder -c "abuild -r"
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,280 @@
-// import the generated boilerplate
 varnish::boilerplate!();
 
-// even though we won't use it here, we still need to know what the context type is
-use varnish::vcl::ctx::Ctx;
+use bytes::Bytes;
+use varnish::vcl::ctx::{Ctx, Event, LogTag, TestCtx};
+use varnish::vcl::vpriv::VPriv;
 
-// this import is only needed for tests
-#[cfg(test)]
-use varnish::vcl::ctx::TestCtx;
-
-// we now implement both functionis from vmod.vcc, but with rust types.
-// Don't forget to make the function public with "pub" in front of them
-
-// we could skip the return, or even use n.is_even(), but let's pace ourselves
-pub fn is_even(_: &Ctx, n: i64) -> bool {
-    return n % 2 == 0;
+pub struct Entry {
+    name: String,
+    transaction: Transaction,
 }
 
-// in vmod.vcc, n was an optional INT, so here it translates into a Option<i64>
-pub fn captain_obvious(_: &Ctx, opt: Option<i64>) -> String {
-    // we need to first "match" to know if a number was provided, if not,
-    // return a default message, otherwise, build a custom one
-    match opt {
-        // no need to return, we are the last expression of the function!
-        None => String::from("I was called without an argument"),
-        // pattern matching FTW!
-        Some(n) => format!("I was given {} as argument", n),
+pub enum Transaction {
+    Transition,
+    Req(reqwest::RequestBuilder),
+    Sent(tokio::sync::mpsc::Receiver<Result<Response, String>>),
+    Resp(Result<Response, String>),
+}
+
+// calling reqwest::Response::body() consumes the object, so we keep a copy of the interesting bits
+// in this struct
+pub struct Response {
+    headers: reqwest::header::HeaderMap,
+    body: Option<Bytes>,
+    status: i64,
+}
+
+impl Transaction {
+    fn unwrap_resp(&self) -> &Result<Response, String> {
+        match self {
+            Transaction::Resp(ref rsp) => rsp,
+            _ => panic!("wrong Transaction type"),
+        }
+    }
+    fn into_req(self) -> reqwest::RequestBuilder {
+        match self {
+            Transaction::Req(rq) => rq,
+            _ => panic!("wrong Transaction type"),
+        }
     }
 }
 
-// Write some more unit tests
-#[test]
-fn obviousness() {
-    let mut test_ctx = TestCtx::new(100);
-    let ctx = test_ctx.ctx();
-
-    assert_eq!(
-        "I was called without an argument",
-        captain_obvious(&ctx, None)
-    );
-    assert_eq!(
-        "I was given 975322 as argument",
-        captain_obvious(&ctx, Some(975322))
-    );
-}
-
-// Write some more unit tests
-#[test]
-fn even_test() {
-    // we don't use it, but we still need one
-    let mut test_ctx = TestCtx::new(100);
-    let ctx = test_ctx.ctx();
-
-    assert_eq!(true, is_even(&ctx, 0));
-    assert_eq!(true, is_even(&ctx, 1024));
-    assert_eq!(false, is_even(&ctx, 421321));
-}
-
-// we also want to run test/test01.vtc
 varnish::vtc!(test01);
+pub fn init(
+    _ctx: &Ctx,
+    vp_vcl: &mut VPriv<BgThread>,
+    vp_task: &mut VPriv<Vec<Entry>>,
+    name: &str,
+    method: &str,
+    url: &str,
+) -> Result<(), String> {
+    if vp_task.as_ref().is_none() {
+        vp_task.store(Vec::new());
+    }
+
+    let ts = vp_task.as_mut().unwrap();
+    let t = Transaction::Req(vp_vcl.as_ref().unwrap().client.request(
+        reqwest::Method::from_bytes(method.as_bytes()).map_err(|e| e.to_string())?,
+        url,
+    ));
+
+    match ts.into_iter().find(|e| e.name == name) {
+        None => ts.push(Entry {
+            transaction: t,
+            name: name.to_owned(),
+        }),
+        Some(e) => e.transaction = t,
+    }
+    Ok(())
+}
+
+fn _send(bgt: &BgThread, t: &mut Transaction) {
+    let sender = bgt.sender.clone();
+    let (tx, rx) = tokio::sync::mpsc::channel(1);
+    let old_t = std::mem::replace(t, Transaction::Sent(rx));
+
+    let _ = sender.send((old_t.into_req(), tx));
+}
+
+fn wait_on(bgt: &BgThread, t: &mut Transaction) -> Result<(), String> {
+    match t {
+        Transaction::Req(_) => {
+            _send(bgt, t);
+            wait_on(bgt, t)
+        }
+        Transaction::Sent(rx) => {
+            *t = Transaction::Resp(rx.blocking_recv().unwrap());
+            Ok(())
+        }
+        Transaction::Resp(_) => Ok(()),
+        Transaction::Transition => panic!("impossible"),
+    }
+}
+
+fn get_transaction<'a, 'b>(
+    vp_task: &'a mut VPriv<Vec<Entry>>,
+    name: &'b str,
+) -> Result<&'a mut Transaction, String> {
+    vp_task
+        .as_mut()
+        .ok_or(format!("reqwest: request \"{}\" isn't initialized", name))?
+        .iter_mut()
+        .find(|e| name == e.name)
+        .map(|e| &mut e.transaction)
+        .ok_or(format!("reqwest: request \"{}\" isn't initialized", name))
+}
+
+pub fn send(
+    _ctx: &Ctx,
+    vp_vcl: &mut VPriv<BgThread>,
+    vp_task: &mut VPriv<Vec<Entry>>,
+    name: &str,
+) -> Result<(), String> {
+    let t = get_transaction(vp_task, name)?;
+
+    match t {
+        Transaction::Req(_) => {
+            _send(vp_vcl.as_ref().unwrap(), t);
+            Ok(())
+        }
+        _ => Err(format!("reqwest: request \"{}\" isn't initialized", name)),
+    }
+}
+
+pub fn exists(_ctx: &Ctx, vp_task: &mut VPriv<Vec<Entry>>, name: &str) -> bool {
+    get_transaction(vp_task, name).is_ok()
+}
+
+pub fn set_header(
+    _ctx: &Ctx,
+    vp_task: &mut VPriv<Vec<Entry>>,
+    name: &str,
+    key: &str,
+    value: &str,
+) -> Result<(), String> {
+    let t = get_transaction(vp_task, name)?;
+    if let Transaction::Req(_) = t {
+        let old_t = std::mem::replace(t, Transaction::Transition);
+        *t = Transaction::Req(old_t.into_req().header(key, value));
+        Ok(())
+    } else {
+        Err(format!("reqwest: request \"{}\" isn't initialized", name))
+    }
+}
+
+pub fn set_body(
+    _ctx: &Ctx,
+    vp_task: &mut VPriv<Vec<Entry>>,
+    name: &str,
+    body: &str,
+) -> Result<(), String> {
+    let t = get_transaction(vp_task, name)?;
+    if let Transaction::Req(_) = t {
+        let old_t = std::mem::replace(t, Transaction::Transition);
+        // it's a bit annoying to have to copy the body, but the request may outlive our task
+        *t = Transaction::Req(old_t.into_req().body(body.to_owned()));
+        Ok(())
+    } else {
+        Err(format!("reqwest: request \"{}\" isn't initialized", name))
+    }
+}
+
+pub fn status(
+    _ctx: &Ctx,
+    vp_vcl: &mut VPriv<BgThread>,
+    vp_task: &mut VPriv<Vec<Entry>>,
+    name: &str,
+) -> Result<i64, String> {
+    let t = get_transaction(vp_task, name)?;
+    wait_on(vp_vcl.as_ref().unwrap(), t)?;
+
+    Ok(t.unwrap_resp().as_ref().map(|rsp| rsp.status).unwrap_or(0))
+}
+
+pub fn header<'a>(
+    _ctx: &Ctx,
+    vp_vcl: &mut VPriv<BgThread>,
+    vp_task: &'a mut VPriv<Vec<Entry>>,
+    name: &str,
+    key: &str,
+) -> Result<Option<&'a [u8]>, String> {
+    let t = get_transaction(vp_task, name)?;
+    wait_on(vp_vcl.as_ref().unwrap(), t)?;
+
+    Ok(t.unwrap_resp()
+        .as_ref()
+        .ok()
+        .map(|rsp| rsp.headers.get(key).map(|h| h.as_ref()))
+        .unwrap_or(None))
+}
+
+pub fn body_as_string<'a>(
+    _ctx: &Ctx,
+    vp_vcl: &mut VPriv<BgThread>,
+    vp_task: &'a mut VPriv<Vec<Entry>>,
+    name: &str,
+) -> Result<&'a [u8], String> {
+    let t = get_transaction(vp_task, name)?;
+    wait_on(vp_vcl.as_ref().unwrap(), t)?;
+
+    match t.unwrap_resp() {
+        Err(_) => Ok("".as_ref()),
+        Ok(rsp) => Ok(rsp.body.as_ref().unwrap()),
+    }
+}
+
+pub fn error(
+    _ctx: &Ctx,
+    vp_vcl: &mut VPriv<BgThread>,
+    vp_task: &mut VPriv<Vec<Entry>>,
+    name: &str,
+) -> Result<Option<String>, String> {
+    let t = get_transaction(vp_task, name)?;
+    wait_on(vp_vcl.as_ref().unwrap(), t)?;
+
+    match t.unwrap_resp() {
+        Err(e) => Ok(Some(e.to_string())),
+        Ok(_) => Ok(None),
+    }
+}
+
+pub struct BgThread {
+    sender: tokio::sync::mpsc::UnboundedSender<(
+        reqwest::RequestBuilder,
+        tokio::sync::mpsc::Sender<Result<Response, String>>,
+    )>,
+    #[allow(dead_code)]
+    rt: tokio::runtime::Runtime,
+    client: reqwest::Client,
+}
+
+pub unsafe fn event(
+    _ctx: &Ctx,
+    vp: &mut VPriv<BgThread>,
+    event: Event,
+) -> Result<(), &'static str> {
+    match event {
+        Event::Load => {
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let (sender, mut receiver) = tokio::sync::mpsc::unbounded_channel::<(
+                reqwest::RequestBuilder,
+                tokio::sync::mpsc::Sender<Result<Response, String>>,
+            )>();
+            let client = reqwest::Client::builder().build().unwrap();
+            rt.spawn(async move {
+                loop {
+                    let (req, tx) = receiver.recv().await.unwrap();
+                    tokio::spawn(async move {
+                        let metadata = match req.send().await {
+                            Err(e) => {
+                                let _ = tx.send(Err(e.to_string())).await;
+                                return;
+                            }
+                            Ok(rsp) => rsp,
+                        };
+                        println!("-------------- {:?}", metadata);
+                        let mut resp = Response {
+                            headers: metadata.headers().clone(),
+                            status: metadata.status().as_u16() as i64,
+                            body: None,
+                        };
+
+                        resp.body = match metadata.bytes().await {
+                            Err(e) => {
+                                let _ = tx.send(Err(e.to_string())).await;
+                                return;
+                            }
+                            Ok(b) => Some(b),
+                        };
+                        // we don't care about the result, somebody may receive our message, or
+                        // not, we don't care
+                        let _ = tx.send(Ok(resp)).await;
+                    });
+                }
+            });
+            vp.store(BgThread { sender, rt, client });
+        }
+        _ => (),
+    }
+    Ok(())
+}
